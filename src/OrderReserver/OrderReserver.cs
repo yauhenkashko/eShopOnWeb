3f1cf5525a902821c3394674150a7bce1605176a
Yauhen Kashko <Yauhen_Kashko@epam.com>
2023-04-02 15:03:01 +0200

    Add logic app http trigger.

--- src/OrderReserver/OrderReserver.cs
+++ src/OrderReserver/OrderReserver.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
+using Microsoft.Azure.Amqp.Framing;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -10,26 +12,66 @@ namespace OrderReserver
 {
     public class OrderReserver
     {
-        [FunctionName("OrderReserver")]
-        public async Task Run([ServiceBusTrigger("reserverqueue", Connection = "OrderReserverQueueConnectionString")] string myQueueItem, string messageId, ILogger log)
-        {
-            log.LogInformation($"ServiceBus queue trigger function processing message: {myQueueItem}");
-            log.LogInformation($"Message: {messageId}");
+        private readonly ILogger _log;
 
+        public OrderReserver(ILogger log)
+        {
+            _log = log;
+        }
+
+        [FunctionName("OrderReserver")]
+        public async Task Run(
+            [ServiceBusTrigger("reserverqueue", Connection = "OrderReserverQueueConnectionString")]
+            string message,
+            string messageId)
+        {
+            _log.LogInformation($"ServiceBus queue trigger function processing message: {message}");
+            _log.LogInformation($"Message: {messageId}");
+
+            try
+            {
+                await SendToBlobStorage(message, messageId);
+
+                _log.LogInformation($"Message has been uploaded to the blob storage: {message}");
+            }
+            catch (Exception e)
+            {
+                _log.LogError($"Error while uploading message to blob storage: {e}");
+                await SendErrorNotification(message);
+            }
+        }
+
+        private async Task SendToBlobStorage(string message, string messageId)
+        {
             var containerName = Environment.GetEnvironmentVariable("BlobStorageContainerName");
             var blobConnectionString = Environment.GetEnvironmentVariable("BlobStorageConnectionString");
 
             var blobServiceClient = new BlobServiceClient(blobConnectionString);
             var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var blobClient = containerClient.GetBlobClient($"{messageId}.json");
 
-            BlobClient blobClient = containerClient.GetBlobClient($"{messageId}.json");
-
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(myQueueItem)))
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(message)))
             {
                 await blobClient.UploadAsync(stream, true);
             }
+        }
 
-            log.LogInformation($"Message has been uploaded to the blob storage: {myQueueItem}");
+        private async Task SendErrorNotification(string message)
+        {
+            var logicAppUrl = Environment.GetEnvironmentVariable("LogicAppUrl");
+
+            using var client = new HttpClient();
+            var requestBody = new StringContent(message, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(logicAppUrl, requestBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _log.LogInformation("Logic App triggered successfully.");
+            }
+            else
+            {
+                _log.LogError($"Failed to trigger Logic App. StatusCode={response.StatusCode}, Reason={response.ReasonPhrase}");
+            }
         }
     }
 }
